9aa7edb9b6b8b458e7d7e88b4c3233f8d2a23e08
adriano2015 <adrianoplane@gmail.com>
2021-11-10 20:11:43 -0300

    script sql banco

---
+++ AgendaDemo/SQLQuery1.sql
@@ -0,0 +1,19 @@
+
+create table contatosAgenda(
+ codContato integer identity (1,1) not null primary key, 
+ NomeContato varchar(39),
+ EnderecoContato varchar(39),
+ TelefonesContato varchar(39),
+ CelularContato varchar(39),
+ EmailContato varchar(39),
+ LinkedinContato varchar(39),
+ CepContato varchar(8),
+ PesquisaContato varchar
+
+
+)
+go
+
+
+
+
