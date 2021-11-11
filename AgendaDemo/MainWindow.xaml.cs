90d8f34fdd518307c80ae01ced4539c162398854
adriano2015 <adrianoplane@gmail.com>
2021-11-10 21:43:17 -0300

    fazendo tratamento de telas

--- AgendaDemo/MainWindow.xaml.cs
+++ AgendaDemo/MainWindow.xaml.cs
@@ -32,5 +32,86 @@ namespace AgendaDemo
             txtCepContato.MaxLength = 8;
             txtPesquisaContato.MaxLength = 39;
         }
+        //*********************************************************************** metodos para tratamento de tela *********************************
+        private void LiparCampos()
+        {
+            txtNomeContato.Clear();
+            txtEnderecoContato.Clear();
+            txtTelefonesContato.Clear();
+            txtCelularContato.Clear();
+            txtEmailContato.Clear();
+            txtLinkedinContato.Clear();
+            txtCepContato.Clear();
+            txtPesquisaContato.Clear();
+
+
+        }
+        private void ControlaBotoes (int opc)
+        {
+            BtnNovoContato.IsEnabled = false;
+            BtnSalvarContato.IsEnabled = false;
+            BtnEditarContato.IsEnabled = false;
+            BtnExcluirContato.IsEnabled = false;
+            BtnCancelaOperacao.IsEnabled = false;
+            BtnProcurarEnderecoContato.IsEnabled = false;
+
+            if(opc == 1)
+            {
+                BtnNovoContato.IsEnabled = true;
+            }
+
+            if(opc == 2)
+            {
+                BtnCancelaOperacao.IsEnabled = true;
+                BtnSalvarContato.IsEnabled = true;
+
+            }
+            if(opc == 3)
+            {
+                BtnEditarContato.IsEnabled = true;
+                BtnExcluirContato.IsEnabled = true;
+                BtnCancelaOperacao.IsEnabled = true;
+
+            }
+
+            
+        }
+        private void ControlaCampos (int opc)
+        {
+            txtNomeContato.IsEnabled = false;
+            txtEnderecoContato.IsEnabled = false;
+            txtTelefonesContato.IsEnabled = false;
+            txtCelularContato.IsEnabled = false;
+            txtEmailContato.IsEnabled = false;
+            txtLinkedinContato.IsEnabled = false;
+            txtCepContato.IsEnabled = false;
+            txtPesquisaContato.IsEnabled = false;
+
+            if(opc == 1)
+            {
+                txtNomeContato.IsEnabled = false;
+                txtEnderecoContato.IsEnabled = false;
+                txtTelefonesContato.IsEnabled = false;
+                txtCelularContato.IsEnabled = false;
+                txtEmailContato.IsEnabled = false;
+                txtLinkedinContato.IsEnabled = false;
+                txtCepContato.IsEnabled = false;
+                txtPesquisaContato.IsEnabled = false;
+
+            }
+            if(opc == 2)
+            {
+                txtNomeContato.IsEnabled = true;
+                txtEnderecoContato.IsEnabled = true;
+                txtTelefonesContato.IsEnabled = true;
+                txtCelularContato.IsEnabled = true;
+                txtEmailContato.IsEnabled = true;
+                txtLinkedinContato.IsEnabled = true;
+                txtCepContato.IsEnabled = true;
+                txtPesquisaContato.IsEnabled = true;
+
+            }
+
+        }
     }
 }
